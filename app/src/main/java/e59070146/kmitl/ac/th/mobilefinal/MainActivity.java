124eaf3c15f2d4ab776b96aa82999e603f2a78e6
rattasart146 <59070146@it.kmitl.ac.th>
2018-12-11 10:33:58 +0700

    Create Project

---
+++ app/src/main/java/e59070146/kmitl/ac/th/mobilefinal/MainActivity.java
@@ -0,0 +1,14 @@
+package e59070146.kmitl.ac.th.mobilefinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
